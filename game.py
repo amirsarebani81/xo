b91a7a2135f7444bc70962495e265341a5800bfc
Amir Hossein Sarebani <amirsarebani1381@gmail.com>
2021-07-28 21:25:51 +0430

    Add cross and circle images to game after click a button

--- game.py
+++ game.py
@@ -11,6 +11,7 @@ import sys
 class Game:
     def __init__(self):
         self.window = tk.Tk()
+        self.labels = list()
         self.button9 = tk.Button(master=self.window, text="9", width=2, height=2, command=self.press_9)
         self.button8 = tk.Button(master=self.window, text="8", width=2, height=2, command=self.press_8)
         self.button7 = tk.Button(master=self.window, text="7", width=2, height=2, command=self.press_7)
@@ -51,7 +52,7 @@ class Game:
         if self.__board.is_board_full():
             self.__game_status = GameStatus.TIE
 
-    def do_step(self, row, column):
+    def do_selects(self, row, column):
         self.__player.mark_square(row, column)
         self.__board.print_board()
         self.__check_game_status()
@@ -69,40 +70,81 @@ class Game:
             sys.exit(0)
 
     def press_1(self):
-        self.do_step(1, 1)
-        self.button1.destroy()
+        self.do_selects(1, 1)
+        self.update_gui()
 
     def press_2(self):
-        self.do_step(1, 2)
-        self.button2.destroy()
+        self.do_selects(1, 2)
+        self.update_gui()
 
     def press_3(self):
-        self.do_step(1, 3)
-        self.button3.destroy()
+        self.do_selects(1, 3)
+        self.update_gui()
 
     def press_4(self):
-        self.do_step(2, 1)
-        self.button4.destroy()
+        self.do_selects(2, 1)
+        self.update_gui()
 
     def press_5(self):
-        self.do_step(2, 2)
-        self.button5.destroy()
+        self.do_selects(2, 2)
+        self.update_gui()
 
     def press_6(self):
-        self.do_step(2, 3)
-        self.button6.destroy()
+        self.do_selects(2, 3)
+        self.update_gui()
 
     def press_7(self):
-        self.do_step(3, 1)
-        self.button7.destroy()
+        self.do_selects(3, 1)
+        self.update_gui()
 
     def press_8(self):
-        self.do_step(3, 2)
-        self.button8.destroy()
+        self.do_selects(3, 2)
+        self.update_gui()
 
     def press_9(self):
-        self.do_step(3, 3)
-        self.button9.destroy()
+        self.do_selects(3, 3)
+        self.update_gui()
+
+    def update_gui(self):
+        cross = tk.PhotoImage(file="cross.png")
+        cross = cross.subsample(50)
+        circle = tk.PhotoImage(file="circle.png")
+        circle = circle.subsample(50)
+        for row in range(1, 4):
+            for column in range(1, 4):
+                if self.__board.get_square(row, column) != SquareStatus.EMPTY:
+                    self.delete_button((row - 1) * 3 + column)
+                    if self.__board.get_square(row, column) == SquareStatus.COMPUTER:
+                        print("cross")
+                        label = tk.Label(master=self.window, image=cross)
+                        label.place(x=(column-1)*50, y=(row-1)*50)
+                        self.labels.append(label)
+                    else:
+                        print("circle")
+                        label = tk.Label(master=self.window, image=circle)
+                        label.place(x=(column-1)*50, y=(row-1)*50)
+                        self.labels.append(label)
+        self.window.mainloop()
+
+    def delete_button(self, button_id):
+        if button_id == 1:
+            self.button1.destroy()
+        if button_id == 2:
+            self.button2.destroy()
+        if button_id == 3:
+            self.button3.destroy()
+        if button_id == 4:
+            self.button4.destroy()
+        if button_id == 5:
+            self.button5.destroy()
+        if button_id == 6:
+            self.button6.destroy()
+        if button_id == 7:
+            self.button7.destroy()
+        if button_id == 8:
+            self.button8.destroy()
+        if button_id == 9:
+            self.button9.destroy()
 
     def run(self):
         if Random().randint(0, 1) == 0:
@@ -118,7 +160,12 @@ class Game:
         self.button8.place(x=50, y=100)
         self.button9.place(x=100, y=100)
 
+        self.update_gui()
         self.window.mainloop()
+
+    def update_status(self):
+        print("updated")
+        self.window.update()
 
     def __print_result(self):
         print('*****************************************')
