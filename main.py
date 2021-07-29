5aa540a209aeda7339702dae708e90de04fc1c30
Amir Hossein Sarebani <amirsarebani1381@gmail.com>
2021-07-29 11:51:30 +0430

    Add start menu to game

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
+import tkinter as tk
 from game import Game
 
-game = Game()
-game.run()
+
+def start_program():
+    start_menu.destroy()
+    game = Game()
+    game.run()
+
+
+start_menu = tk.Tk()
+start_button = tk.Button(master=start_menu, text="Start", command=start_program)
+start_button.pack()
+
+start_menu.mainloop()
