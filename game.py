eaa4fe6187e868df2f6037268f086fa93211097a
Amir Hossein Sarebani <amirsarebani1381@gmail.com>
2021-07-29 21:11:46 +0430

    Refactor game.py

--- game.py
+++ game.py
@@ -19,75 +19,75 @@ class Game:
         self.__game_status = GameStatus.CONTINUE
 
     def __init_window(self):
-        self.window = tk.Tk()
-        self.window.title(self.__computer.get_game_level())
-        self.window.geometry("400x400")
-        self.window.config(bg="#272927")
-        self.window.minsize(width=400, height=400)
-        self.window.maxsize(width=400, height=400)
+        self.__window = tk.Tk()
+        self.__window.title(self.__computer.get_game_level())
+        self.__window.geometry("400x400")
+        self.__window.config(bg="#272927")
+        self.__window.minsize(width=400, height=400)
+        self.__window.maxsize(width=400, height=400)
 
     def __init_buttons(self):
-        self.buttons = list()
-        self.buttons.append(
-            tk.Button(master=self.window, text=1, width=5, height=4, background="#272927", highlightthickness=0, bd=0,
+        self.__buttons = list()
+        self.__buttons.append(
+            tk.Button(master=self.__window, text=1, width=5, height=4, background="#272927", highlightthickness=0, bd=0,
                       command=self.__press_1))
-        self.buttons.append(
-            tk.Button(master=self.window, text=2, width=5, height=4, background="#272927", highlightthickness=0, bd=0,
+        self.__buttons.append(
+            tk.Button(master=self.__window, text=2, width=5, height=4, background="#272927", highlightthickness=0, bd=0,
                       command=self.__press_2))
-        self.buttons.append(
-            tk.Button(master=self.window, text=3, width=5, height=4, background="#272927", highlightthickness=0, bd=0,
+        self.__buttons.append(
+            tk.Button(master=self.__window, text=3, width=5, height=4, background="#272927", highlightthickness=0, bd=0,
                       command=self.__press_3))
-        self.buttons.append(
-            tk.Button(master=self.window, text=4, width=5, height=4, background="#272927", highlightthickness=0, bd=0,
+        self.__buttons.append(
+            tk.Button(master=self.__window, text=4, width=5, height=4, background="#272927", highlightthickness=0, bd=0,
                       command=self.__press_4))
-        self.buttons.append(
-            tk.Button(master=self.window, text=5, width=5, height=4, background="#272927", highlightthickness=0, bd=0,
+        self.__buttons.append(
+            tk.Button(master=self.__window, text=5, width=5, height=4, background="#272927", highlightthickness=0, bd=0,
                       command=self.__press_5))
-        self.buttons.append(
-            tk.Button(master=self.window, text=6, width=5, height=4, background="#272927", highlightthickness=0, bd=0,
+        self.__buttons.append(
+            tk.Button(master=self.__window, text=6, width=5, height=4, background="#272927", highlightthickness=0, bd=0,
                       command=self.__press_6))
-        self.buttons.append(
-            tk.Button(master=self.window, text=7, width=5, height=4, background="#272927", highlightthickness=0, bd=0,
+        self.__buttons.append(
+            tk.Button(master=self.__window, text=7, width=5, height=4, background="#272927", highlightthickness=0, bd=0,
                       command=self.__press_7))
-        self.buttons.append(
-            tk.Button(master=self.window, text=8, width=5, height=4, background="#272927", highlightthickness=0, bd=0,
+        self.__buttons.append(
+            tk.Button(master=self.__window, text=8, width=5, height=4, background="#272927", highlightthickness=0, bd=0,
                       command=self.__press_8))
-        self.buttons.append(
-            tk.Button(master=self.window, text=9, width=5, height=4, background="#272927", highlightthickness=0, bd=0,
+        self.__buttons.append(
+            tk.Button(master=self.__window, text=9, width=5, height=4, background="#272927", highlightthickness=0, bd=0,
                       command=self.__press_9))
 
-        self.buttons[0].place(relx=0.25, rely=0.25, anchor=CENTER)
-        self.buttons[1].place(relx=0.5, rely=0.25, anchor=CENTER)
-        self.buttons[2].place(relx=0.75, rely=0.25, anchor=CENTER)
-        self.buttons[3].place(relx=0.25, rely=0.5, anchor=CENTER)
-        self.buttons[4].place(relx=0.5, rely=0.5, anchor=CENTER)
-        self.buttons[5].place(relx=0.75, rely=0.5, anchor=CENTER)
-        self.buttons[6].place(relx=0.25, rely=0.75, anchor=CENTER)
-        self.buttons[7].place(relx=0.5, rely=0.75, anchor=CENTER)
-        self.buttons[8].place(relx=0.75, rely=0.75, anchor=CENTER)
+        self.__buttons[0].place(relx=0.25, rely=0.25, anchor=CENTER)
+        self.__buttons[1].place(relx=0.5, rely=0.25, anchor=CENTER)
+        self.__buttons[2].place(relx=0.75, rely=0.25, anchor=CENTER)
+        self.__buttons[3].place(relx=0.25, rely=0.5, anchor=CENTER)
+        self.__buttons[4].place(relx=0.5, rely=0.5, anchor=CENTER)
+        self.__buttons[5].place(relx=0.75, rely=0.5, anchor=CENTER)
+        self.__buttons[6].place(relx=0.25, rely=0.75, anchor=CENTER)
+        self.__buttons[7].place(relx=0.5, rely=0.75, anchor=CENTER)
+        self.__buttons[8].place(relx=0.75, rely=0.75, anchor=CENTER)
 
     def __init_labels(self):
-        self.labels = list()
+        self.__labels = list()
 
     def __init_images(self):
-        self.images = dict()
-        self.images['cross'] = tk.PhotoImage(file="images/cross.png")
-        self.images['circle'] = tk.PhotoImage(file="images/circle.png")
-        self.images['win'] = tk.PhotoImage(file="images/win.png")
-        self.images['lose'] = tk.PhotoImage(file="images/lose.png")
-        self.images['tie'] = tk.PhotoImage(file="images/tie.png")
-        self.images['restart'] = tk.PhotoImage(file="images/restart.png")
-        self.images['menu'] = tk.PhotoImage(file="images/menu.png")
-        self.images['exit'] = tk.PhotoImage(file="images/exit.png")
+        self.__images = dict()
+        self.__images['cross'] = tk.PhotoImage(file="images/cross.png")
+        self.__images['circle'] = tk.PhotoImage(file="images/circle.png")
+        self.__images['win'] = tk.PhotoImage(file="images/win.png")
+        self.__images['lose'] = tk.PhotoImage(file="images/lose.png")
+        self.__images['tie'] = tk.PhotoImage(file="images/tie.png")
+        self.__images['restart'] = tk.PhotoImage(file="images/restart.png")
+        self.__images['menu'] = tk.PhotoImage(file="images/menu.png")
+        self.__images['exit'] = tk.PhotoImage(file="images/exit.png")
 
-        self.images['cross'] = self.images['cross'].subsample(30)
-        self.images['circle'] = self.images['circle'].subsample(18)
-        self.images['win'] = self.images['win'].subsample(5)
-        self.images['lose'] = self.images['lose'].subsample(5)
-        self.images['tie'] = self.images['tie'].subsample(2)
-        self.images['restart'] = self.images['restart'].subsample(15)
-        self.images['menu'] = self.images['menu'].subsample(15)
-        self.images['exit'] = self.images['exit'].subsample(60)
+        self.__images['cross'] = self.__images['cross'].subsample(30)
+        self.__images['circle'] = self.__images['circle'].subsample(18)
+        self.__images['win'] = self.__images['win'].subsample(5)
+        self.__images['lose'] = self.__images['lose'].subsample(5)
+        self.__images['tie'] = self.__images['tie'].subsample(2)
+        self.__images['restart'] = self.__images['restart'].subsample(15)
+        self.__images['menu'] = self.__images['menu'].subsample(15)
+        self.__images['exit'] = self.__images['exit'].subsample(60)
 
     def __check_game_status(self):
         for row in range(1, 4):
@@ -116,9 +116,7 @@ class Game:
             self.__game_status = GameStatus.TIE
 
     def __do_selects(self, row, column):
-        print(self.__computer.get_game_level())
         self.__player.mark_square(row, column)
-        self.__board.print_board()
         self.__check_game_status()
 
         if self.__game_status != GameStatus.CONTINUE:
@@ -127,10 +125,9 @@ class Game:
             self.__init_restart_button()
             self.__init_menu_button()
             self.__init_exit_button()
-            self.window.mainloop()
+            self.__window.mainloop()
 
         self.__computer.select_square()
-        self.__board.print_board()
         self.__check_game_status()
 
         if self.__game_status != GameStatus.CONTINUE:
@@ -139,7 +136,7 @@ class Game:
             self.__init_restart_button()
             self.__init_menu_button()
             self.__init_exit_button()
-            self.window.mainloop()
+            self.__window.mainloop()
 
     def __press_1(self):
         self.__do_selects(1, 1)
@@ -183,44 +180,44 @@ class Game:
                 if self.__board.get_square(row, column) != SquareStatus.EMPTY:
                     self.__delete_button((row - 1) * 3 + column)
                     if self.__board.get_square(row, column) == SquareStatus.COMPUTER:
-                        label = tk.Label(master=self.window, image=self.images['cross'], background="#272927")
+                        label = tk.Label(master=self.__window, image=self.__images['cross'], background="#272927")
                         label.place(relx=column * 0.25, rely=row * 0.25, anchor=CENTER)
-                        self.labels.append(label)
+                        self.__labels.append(label)
                     else:
-                        label = tk.Label(master=self.window, image=self.images['circle'], background="#272927")
+                        label = tk.Label(master=self.__window, image=self.__images['circle'], background="#272927")
                         label.place(relx=column * 0.25, rely=row * 0.25, anchor=CENTER)
-                        self.labels.append(label)
-        self.window.mainloop()
+                        self.__labels.append(label)
+        self.__window.mainloop()
 
     def __delete_button(self, button_id):
-        self.buttons[button_id - 1].destroy()
+        self.__buttons[button_id - 1].destroy()
 
-    def start_hard_game(self):
-        self.start_menu.destroy()
+    def __start_hard_game(self):
+        self.__start_menu.destroy()
         self.__computer.set_game_level(GameLevel.HARD)
-        self.run()
+        self.__run()
 
-    def start_medium_game(self):
-        self.start_menu.destroy()
+    def __start_medium_game(self):
+        self.__start_menu.destroy()
         self.__computer.set_game_level(GameLevel.MEDIUM)
-        self.run()
+        self.__run()
 
-    def start_easy_game(self):
-        self.start_menu.destroy()
+    def __start_easy_game(self):
+        self.__start_menu.destroy()
         self.__computer.set_game_level(GameLevel.EASY)
-        self.run()
+        self.__run()
 
     def __init_menu(self):
-        self.start_menu = tk.Tk()
-        self.start_menu.title("XO")
-        self.start_menu.geometry("400x400")
-        self.start_menu.config(bg="#272927")
-        self.start_menu.minsize(width=400, height=400)
-        self.start_menu.maxsize(width=400, height=400)
+        self.__start_menu = tk.Tk()
+        self.__start_menu.title("XO")
+        self.__start_menu.geometry("400x400")
+        self.__start_menu.config(bg="#272927")
+        self.__start_menu.minsize(width=400, height=400)
+        self.__start_menu.maxsize(width=400, height=400)
 
-        easy = tk.Button(master=self.start_menu, text="easy", command=self.start_easy_game)
-        medium = tk.Button(master=self.start_menu, text="medium", command=self.start_medium_game)
-        hard = tk.Button(master=self.start_menu, text="hard", command=self.start_hard_game)
+        easy = tk.Button(master=self.__start_menu, text="easy", command=self.__start_easy_game)
+        medium = tk.Button(master=self.__start_menu, text="medium", command=self.__start_medium_game)
+        hard = tk.Button(master=self.__start_menu, text="hard", command=self.__start_hard_game)
 
         easy.config(width=20, height=2)
         medium.config(width=20, height=2)
@@ -232,9 +229,9 @@ class Game:
 
     def run_menu(self):
         self.__init_menu()
-        self.start_menu.mainloop()
+        self.__start_menu.mainloop()
 
-    def run(self):
+    def __run(self):
         self.__init_window()
         self.__init_buttons()
         self.__init_labels()
@@ -245,52 +242,52 @@ class Game:
             self.__computer.select_square()
 
         self.__update_gui()
-        self.window.mainloop()
+        self.__window.mainloop()
 
     def __clean_window(self):
-        for label in self.labels:
+        for label in self.__labels:
             label.destroy()
-        self.labels.clear()
+        self.__labels.clear()
 
-        for button in self.buttons:
+        for button in self.__buttons:
             button.destroy()
-        self.buttons.clear()
+        self.__buttons.clear()
 
     def __print_result(self):
-        label = tk.Label(master=self.window, background="#272927")
+        label = tk.Label(master=self.__window, background="#272927")
         if self.__game_status == GameStatus.PLAYER_WIN:
-            label.config(image=self.images['win'])
+            label.config(image=self.__images['win'])
         if self.__game_status == GameStatus.COMPUTER_WIN:
-            label.config(image=self.images['lose'])
+            label.config(image=self.__images['lose'])
         if self.__game_status == GameStatus.TIE:
-            label.config(image=self.images['tie'])
+            label.config(image=self.__images['tie'])
 
         label.place(relx=0.5, rely=0.5, anchor=CENTER)
-        self.labels.append(label)
+        self.__labels.append(label)
 
     def __init_restart_button(self):
-        restart_button = tk.Button(master=self.window, image=self.images['restart'], width=0, height=0,
+        restart_button = tk.Button(master=self.__window, image=self.__images['restart'], width=0, height=0,
                                    background="#272927", foreground="#f5d442", highlightthickness=0, bd=0,
-                                   command=self.restart)
+                                   command=self.__restart)
         restart_button.place(relx=0.7, rely=0.9, anchor=CENTER)
-        self.buttons.append(restart_button)
+        self.__buttons.append(restart_button)
 
     def __init_menu_button(self):
-        menu_button = tk.Button(master=self.window, image=self.images['menu'], width=0, height=0,
+        menu_button = tk.Button(master=self.__window, image=self.__images['menu'], width=0, height=0,
                                 background="#272927", foreground="#f5d442", highlightthickness=0, bd=0,
                                 command=self.__press_menu_button)
         menu_button.place(relx=0.3, rely=0.9, anchor=CENTER)
-        self.buttons.append(menu_button)
+        self.__buttons.append(menu_button)
 
     def __init_exit_button(self):
-        exit_button = tk.Button(master=self.window, image=self.images['exit'], width=0, height=0,
+        exit_button = tk.Button(master=self.__window, image=self.__images['exit'], width=0, height=0,
                                 background="#272927", foreground="#f5d442", highlightthickness=0, bd=0,
                                 command=sys.exit)
         exit_button.place(relx=0.5, rely=0.9, anchor=CENTER)
-        self.buttons.append(exit_button)
+        self.__buttons.append(exit_button)
 
     def __press_menu_button(self):
-        self.window.destroy()
+        self.__window.destroy()
         self.run_menu()
 
     @staticmethod
@@ -301,7 +298,7 @@ class Game:
             return GameStatus.PLAYER_WIN
         return GameStatus.CONTINUE
 
-    def restart(self):
+    def __restart(self):
         self.__board.clean_board()
-        self.window.destroy()
-        self.run()
+        self.__window.destroy()
+        self.__run()
