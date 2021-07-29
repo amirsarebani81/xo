81c6822c1ca02132adc217a2aa4a225bb0213aef
Amir Hossein Sarebani <amirsarebani1381@gmail.com>
2021-07-29 14:16:08 +0430

    Update start menu for add levels

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
 import tkinter as tk
+from constants.game_level import GameLevel
 from game import Game
 
 
-def start_program():
+def start_hard_game():
     start_menu.destroy()
-    game = Game()
+    game = Game(GameLevel.HARD)
+    game.run()
+
+
+def start_medium_game():
+    start_menu.destroy()
+    game = Game(GameLevel.MEDIUM)
+    game.run()
+
+
+def start_easy_game():
+    start_menu.destroy()
+    game = Game(GameLevel.EASY)
     game.run()
 
 
 start_menu = tk.Tk()
-start_button = tk.Button(master=start_menu, text="Start", command=start_program)
-start_button.pack()
+easy = tk.Button(master=start_menu, text="easy", command=start_easy_game)
+medium = tk.Button(master=start_menu, text="medium", command=start_medium_game)
+hard = tk.Button(master=start_menu, text="hard", command=start_hard_game)
+easy.pack()
+medium.pack()
+hard.pack()
 
 start_menu.mainloop()
